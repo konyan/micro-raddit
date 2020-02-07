76a4742f3ece6ed3e11b875d26a0bd0c277281eb
konyan <devkonyan@gmail.com>
2020-02-08 00:27:56 +0630

    add db:migrate for comment model

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,15 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_02_07_170543) do
+ActiveRecord::Schema.define(version: 2020_02_07_175646) do
+
+  create_table "comments", force: :cascade do |t|
+    t.string "text"
+    t.integer "user_id"
+    t.integer "post_id"
+    t.datetime "created_at", precision: 6, null: false
+    t.datetime "updated_at", precision: 6, null: false
+  end
 
   create_table "posts", force: :cascade do |t|
     t.string "title"
