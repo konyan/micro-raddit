053169795a2f7eea73c0269b1fdfb615a9f4df7e
konyan <devkonyan@gmail.com>
2020-02-08 00:43:07 +0630

    write testing about comment and validation

--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,2 +1,9 @@
 class Comment < ApplicationRecord
+
+  belongs_to :user
+  belongs_to :post
+
+  validates :text,presence: true, length: { minimum: 5 }
+  validates :user_id,:post_id, presence: true
+
 end
--- test/models/comment_test.rb
+++ test/models/comment_test.rb
@@ -1,7 +1,24 @@
 require 'test_helper'
 
 class CommentTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  def setup
+    @user = User.create(name:"nyan" , email: "nyan@gmail.com")
+    @post = Post.create(title:"hello world title", body: "this is large body",            user_id:@user.id)
+    @comment = Comment.new(text: "greate post",user_id:@user.id, post_id:@post.id)
+  end
+
+  test 'comment should be valid' do
+    assert @comment.valid?
+  end
+
+  test 'comment text should not be blank' do
+    @comment.text = " "
+    assert_not @comment.valid?
+  end
+
+  test "comment text at least 5 charactor" do
+    @comment.text = "a" * 4
+    assert_not @comment.valid?
+  end
+
 end
